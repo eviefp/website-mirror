c681b4cda663c6ff611eaac0c849dd30913dc9b6
Evie Ciobanu <me@evie.ro>
2025-07-20 20:53:48 +0300

    sample, generated, etc.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Quickly getting started
+
+```sh
+nix develop # enter the shell with the static generator
+eviero # yes, this is the static gen name
+# ^ repeat as needed when changing the static files
+http-server . # start a http server to view changes
+```
---
+++ flake.nix
@@ -0,0 +1,63 @@
+{
+  description = "Group Meowing website";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    evie-blog-engine = {
+      url = "github:eviefp/website-engine?ref=blogv1";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    pico-css = {
+      url = "github:picocss/pico?ref=main";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, treefmt-nix, evie-blog-engine, pico-css }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+          treefmt-config = {
+            projectRootFile = "flake.nix";
+            programs = {
+              nixpkgs-fmt.enable = true;
+              cabal-fmt.enable = true;
+              fourmolu.enable = true;
+              fourmolu.package = pkgs.haskell.packages.ghc984.fourmolu;
+            };
+          };
+          treefmt = (treefmt-nix.lib.evalModule pkgs treefmt-config).config.build;
+        in
+        {
+          formatter = treefmt.wrapper;
+
+          checks = {
+            fmt = treefmt.check self;
+            hlint = pkgs.runCommand "hlint" { buildInputs = [ pkgs.hlint ]; } ''
+              cd ${./.}
+              hlint src spec app
+              touch $out
+            '';
+          };
+
+          devShells.default = pkgs.mkShell {
+            name = "eviero-shell";
+            buildInputs = [
+              evie-blog-engine.packages.x86_64-linux.default
+              pkgs.http-server
+            ];
+            shellHook = ''
+              ln -sf ${pico-css}/css/pico.min.css site/css/pico.min.css
+            '';
+          };
+        }
+      );
+}
