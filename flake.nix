29f2e6f69b8036c4391026567db2a22256167ed7
Evie Ciobanu <me@evie.ro>
2025-11-19 12:42:44 +0100

    move flake input to codeberg (#48)
    sneaked in a bit of nix formatting/change
    
    Reviewed-on: https://codeberg.org/group-meowing/website/pulls/48
    Co-authored-by: Evie Ciobanu <me@evie.ro>
    Co-committed-by: Evie Ciobanu <me@evie.ro>

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     website-engine = {
-      url = "github:eviefp/website-engine";
+      url = "git+https://codeberg.org/eviefp/website-engine.git";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     treefmt-nix = {
@@ -18,127 +18,137 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, treefmt-nix, website-engine, pico-css }:
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    treefmt-nix,
+    website-engine,
+    pico-css,
+  }:
     flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          # use this for tooling, higher chances of nixos cache hits
-          pkgs = import nixpkgs {
-            inherit system;
-          };
+    (
+      system: let
+        # use this for tooling, higher chances of nixos cache hits
+        pkgs = import nixpkgs {
+          inherit system;
+        };
 
-          # use this for haskellPackages
-          pkgs-pandoc-upgrade = import nixpkgs {
-            inherit system;
-            overlays = [ website-engine.overlays.default ];
-          };
+        # use this for haskellPackages
+        pkgs-pandoc-upgrade = import nixpkgs {
+          inherit system;
+          overlays = [website-engine.overlays.default];
+        };
 
-          # add our current package to the list;
-          haskellPackages = pkgs-pandoc-upgrade.haskell.packages.ghc9102.override (prevArgs: {
-            # warning: we need to `composeExtensions` because we also have the overlay that overrides this bit
-            overrides = pkgs-pandoc-upgrade.lib.composeExtensions (prevArgs.overrides or (_: _: { })) (final: prev: {
-              group-meowing = final.callCabal2nix "group-meowing" ./. { };
-            });
+        # add our current package to the list;
+        haskellPackages = pkgs-pandoc-upgrade.haskell.packages.ghc9102.override (prevArgs: {
+          # warning: we need to `composeExtensions` because we also have the overlay that overrides this bit
+          overrides = pkgs-pandoc-upgrade.lib.composeExtensions (prevArgs.overrides or (_: _: {})) (final: _prev: {
+            group-meowing = final.callCabal2nix "group-meowing" ./. {};
           });
+        });
 
-          # helper to create nix apps
-          mkCommand = runtimeInputs: text: {
-            type = "app";
-            program = pkgs.lib.getExe (pkgs.writeShellApplication {
-              name = "command";
-              inherit runtimeInputs text;
-            });
-          };
+        # helper to create nix apps
+        mkCommand = runtimeInputs: text: {
+          type = "app";
+          program = pkgs.lib.getExe (pkgs.writeShellApplication {
+            name = "command";
+            inherit runtimeInputs text;
+          });
+        };
 
-          group-meowing = haskellPackages.callCabal2nix "group-meowing" ./. { };
+        group-meowing = haskellPackages.callCabal2nix "group-meowing" ./. {};
 
-          # formatter config
-          treefmt-config = {
-            projectRootFile = "flake.nix";
-            programs = {
-              nixpkgs-fmt.enable = true;
-              cabal-fmt.enable = true;
-              fourmolu.enable = true;
-              fourmolu.package = pkgs.haskell.packages.ghc9102.fourmolu;
+        # formatter config
+        treefmt-config = {
+          projectRootFile = "flake.nix";
+          programs = {
+            alejandra.enable = true;
+            cabal-fmt.enable = true;
+            deadnix.enable = true;
+            fourmolu = {
+              enable = true;
+              package = pkgs.haskell.packages.ghc9102.fourmolu;
             };
+            hlint.enable = true;
+            jsonfmt.enable = true;
           };
-          treefmt = (treefmt-nix.lib.evalModule pkgs treefmt-config).config.build;
-        in
-        {
-          # run via `nix run .#build` / `nix run .#generate`
-          apps = {
-            build = (mkCommand [ ] ''
-              cabal build group-meowing
-            '');
-            generate = (mkCommand [ ] ''
-              ${pkgs.lib.getExe (group-meowing)}
-            '');
-            clean = (mkCommand [ ] ''
-              ${pkgs.lib.getExe (group-meowing)} clean
-            '');
-            repl = (mkCommand [ ] ''
-              cabal v2-repl group-meowing
-            '');
-            http-server = (mkCommand [ ] ''
-              http-server docs
-            '');
-            browse-site = (mkCommand [ ] ''
-              xdg-open http://localhost:8080
-            '');
-            hoogle = (mkCommand [ ] ''
-              hoogle server --local --port 8999
-            '');
-            browse-hoogle = (mkCommand [ ] ''
-              xdg-open http://localhost:8999
-            '');
-            help = (mkCommand [ ] ''
-              ${pkgs.lib.getExe (group-meowing)} --help
-            '');
-          };
+        };
+        treefmt = (treefmt-nix.lib.evalModule pkgs treefmt-config).config.build;
+      in {
+        # run via `nix run .#build` / `nix run .#generate`
+        apps = {
+          build = mkCommand [] ''
+            cabal build group-meowing
+          '';
+          generate = mkCommand [] ''
+            ${pkgs.lib.getExe group-meowing}
+          '';
+          clean = mkCommand [] ''
+            ${pkgs.lib.getExe group-meowing} clean
+          '';
+          repl = mkCommand [] ''
+            cabal v2-repl group-meowing
+          '';
+          http-server = mkCommand [] ''
+            http-server docs
+          '';
+          browse-site = mkCommand [] ''
+            xdg-open http://localhost:8080
+          '';
+          hoogle = mkCommand [] ''
+            hoogle server --local --port 8999
+          '';
+          browse-hoogle = mkCommand [] ''
+            xdg-open http://localhost:8999
+          '';
+          help = mkCommand [] ''
+            ${pkgs.lib.getExe group-meowing} --help
+          '';
+        };
 
-          # run with `nix fmt`
-          formatter = treefmt.wrapper;
+        # run with `nix fmt`
+        formatter = treefmt.wrapper;
 
-          # run with `nix flake check`
-          checks = {
-            # run the formatter in check mode (errors instead of fixing)
-            fmt = treefmt.check self;
+        # run with `nix flake check`
+        checks = {
+          # run the formatter in check mode (errors instead of fixing)
+          fmt = treefmt.check self;
 
-            # haskell lints
-            hlint = pkgs.runCommand "hlint" { buildInputs = [ pkgs.hlint ]; } ''
-              cd ${./.}
-              hlint app
-              touch $out
-            '';
+          # haskell lints
+          hlint = pkgs.runCommand "hlint" {buildInputs = [pkgs.hlint];} ''
+            cd ${./.}
+            hlint app
+            touch $out
+          '';
 
-            # by building the project, nix also runs the tests automatically
-            hstests = pkgs.runCommand "hstests" { buildInputs = [ group-meowing ]; } ''
-              touch $out
-            '';
-          };
+          # by building the project, nix also runs the tests automatically
+          hstests = pkgs.runCommand "hstests" {buildInputs = [group-meowing];} ''
+            touch $out
+          '';
+        };
 
-          # run with `nix run .#default`
-          packages.default = group-meowing;
+        # run with `nix run .#default`
+        packages.default = group-meowing;
 
-          # run with `nix develop`
-          devShells.default = haskellPackages.shellFor {
-            packages = p: [ p.group-meowing ];
-            # This is a bit slow.
-            withHoogle = false;
-            buildInputs = [
-              pkgs.haskell.compiler.ghc9102
+        # run with `nix develop`
+        devShells.default = haskellPackages.shellFor {
+          packages = p: [p.group-meowing];
+          # This is a bit slow.
+          withHoogle = false;
+          buildInputs = [
+            pkgs.haskell.compiler.ghc9102
 
-              pkgs.haskell.packages.ghc9102.cabal-install
-              pkgs.haskell.packages.ghc9102.haskell-language-server
+            pkgs.haskell.packages.ghc9102.cabal-install
+            pkgs.haskell.packages.ghc9102.haskell-language-server
 
-              pkgs.http-server
-              pkgs.zlib.dev
-            ];
-            shellHook = ''
-              ln - sf ${pico-css}/css/pico.min.css site/css/pico.min.css
-            '';
-          };
-        }
-      );
+            pkgs.http-server
+            pkgs.zlib.dev
+          ];
+          shellHook = ''
+            ln - sf ${pico-css}/css/pico.min.css site/css/pico.min.css
+          '';
+        };
+      }
+    );
 }
-
